5b01bf9b663d1f019a72625a2bb559a7bb9b5187
liu1597 <liu1597@flinders.edu.au>
2024-08-17 23:58:54 +0930

    add redux

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,19 +1,33 @@
 "use client"
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
 import { Menu } from 'lucide-react'
 import React from 'react'
 
 const Sidebar = () => {
-  return (
-    <div>
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed);
+
+    const toggleSidebar = () =>{
+      dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+    };
+
+    const sidebarClassNames = `fixed flex-col ${
+      isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+    } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
+    return (
+    <div className={sidebarClassNames}>
 
         {/* Top logo */}
         <div className='flex gap-3 justify-between md:justify-between items-center pt-8'>
             <div>logo</div>
-            <h1 className=' font-extrabold text-2xl'>JustInventory</h1>
+            <h1 className=' font-extrabold text-xl'>JQ</h1>
        
         <button 
           className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' 
-          onClick={() =>{}}>
+          onClick={toggleSidebar}>
           <Menu className='w-4 h-4' />
         </button>
         </div>
@@ -25,7 +39,7 @@ const Sidebar = () => {
 
         {/* Footer */}
         <div>
-            <p className='text-center text-xs text-gray-500'>© 2024 JustInventory</p>
+            <p className='text-center text-xs text-gray-500'>© 2024 JQ</p>
         </div>
     </div>
   )
